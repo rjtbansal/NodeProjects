6281d0621c176a6e4268ac461e52fa88d1a578f6
rjtbansal <rjtbansal@yahoo.com>
2016-07-05 15:10:29 -0700

    able to display saved posts

--- nodeblog/routes/index.js
+++ nodeblog/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongo=require('mongodb');
+var db=require('monk')('localhost/nodeblog'); //monk alternative to mongoose for mongodb
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var db=req.db; //getting connection
+  var posts=db.get('posts'); //getting posts collection
+  posts.find({},{},function(err, posts){
+      res.render('index', { posts: posts }); //passing the collection to index
+  });
 });
 
 module.exports = router;
