39adc5ae8e72e183c82785e369de3ed7788b7793
rjtbansal <rjtbansal@yahoo.com>
2016-07-06 11:34:09 -0700

    completed form and populating categories dynamically

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -16,7 +16,7 @@ var mongo=require('mongodb');
 var db=require('monk')('localhost/nodeblog'); //monk alternative to mongoose for mongodb
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var posts = require('./routes/posts');
 
 var app = express();
 
@@ -74,7 +74,7 @@ app.use(expressValidator({
 }));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ nodeblog/routes/posts.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' }); //for file uploads
+var mongo=require('mongodb');
+var db=require('monk')('localhost/nodeblog'); //monk alternative to mongoose for mongodb..nodeblog is db name
+
+// endpoint: /posts/add
+router.get('/add', function(req, res, next) {
+  
+  //extracting out categories collection
+  var categories=db.get('categories');
+  
+  //passing categories to our function 
+  categories.find({},{},function(err, categories){
+  res.render('addpost',{
+    'title':'Add Post',
+    'categories':categories //passing the collection to addpost page
+  });
+  });
+});
+
+//endpoint : /posts/add 
+router.post('/add',upload.single('mainimage'),function(req,res,next){
+  
+  //extracting form data
+  var title=req.body.title; 
+  var category=req.body.category;
+  var body=req.body.body;
+  var author=req.body.author;
+  
+  //date will be calculated and sent right then
+  var date=new Date();
+  
+  //check to see if file is uploaded 
+  if(req.file){
+    console.log("file uploaded");
+    var mainimage=req.file.filename;
+  }else{
+    console.log('File not uploaded');
+    var mainimage='noimage.jpg';
+  }  
+  
+  //form validation
+  req.checkBody('title','Title field is required').notEmpty();
+  req.checkBody('body','Body field is required').notEmpty();
+ 
+ //check for errors
+  var errors = req.validationErrors();
+  
+  if (errors) {
+    res.render('addpost',{
+      "errors":errors
+    });
+  }else{
+    //getting our posts collection
+    var posts=db.get('posts');
+    
+     posts.insert({
+       "title":title,
+       "body":body,
+       "category":category,
+       "author":author,
+       "date":date,
+       "mainimage":mainimage
+     }, function(err,post){
+        if(err){
+          res.send(err);
+        }else{
+          req.flash('success','Post added successfully');
+          res.location('/');
+          res.redirect('/');
+        }
+     });
+  } 
+});
+
+module.exports = router;
