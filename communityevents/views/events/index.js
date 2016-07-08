5d08c254bd18b7a2f845a1177acf6104753122d1
rjtbansal <rjtbansal@yahoo.com>
2016-07-08 13:23:36 -0700

    event add and display working

--- communityevents/views/events/index.js
+++ communityevents/views/events/index.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 exports.find=function(req,res,next) {
   req.query.name = req.query.name ? req.query.name : '';
@@ -33,3 +33,64 @@ exports.find=function(req,res,next) {
     }
   });
 };
+
+exports.read=function(req,res,next) {
+    req.app.db.models.Event.findById(req.params.id).exec(function(err, event){
+        if(err){
+            return next(err);
+        }
+        if(req.xhr){
+            res.send(event);
+        }else{
+            res.render('events/details',{event:event});
+        }
+    });
+};
+
+exports.add=function(req,res){
+    if(!req.isAuthenticated()){
+        req.flash('error','You are not logged in');
+        res.location('/events');
+        res.redirect('/events');
+    }
+    res.render('events/add');
+};
+
+//using event emitters: useful for CRUD
+exports.create=function(req,res,next) {
+    var workflow= req.app.utility.workflow(req,res);
+
+    workflow.on('validate',function() {
+       if(!req.body.name){
+           workflow.outcome.errors.push('Please enter a name');
+           return workflow.emit('response');
+       } 
+       workflow.emit('createEvent');
+    });
+
+    workflow.on('createEvent',function(){
+        var fieldsToSet={
+            name: req.body.name,
+            description: req.body.menu,
+            venue: req.body.venue,
+            date: req.body.date,
+            startTime: req.body.startTime,
+            endTime: req.body.endTime,
+            username: req.user.username
+        };
+
+        req.app.db.models.Event.create(fieldsToSet,function(err,event) {
+           if(err){
+               return workflow.emit('exception',err);
+           } 
+
+           workflow.outcome.record=event;
+           req.flash('success','Event Added!');
+           res.location('/events');
+           res.redirect('/events');
+        });
+    });
+
+    workflow.emit('validate');
+};
+
