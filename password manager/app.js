a538a76ca4a7ad7703cd7d629feb4fc6646d0f41
rjtbansal <rjtbansal@yahoo.com>
2016-07-30 21:15:06 -0700

    added createaccount and getaccount functions

--- password manager/app.js
+++ password manager/app.js
@@ -11,7 +11,38 @@ storage.initSync(); //to initialize variable storage
 //storage.setItemSync('accounts',[{username: 'rajat', balance:20000}]);
 
 //storage.setItemSync('accounts',[{username:'raghav',balance:10000}]);
-var accounts=storage.getItemSync('accounts');
+//var accounts=storage.getItemSync('accounts');
 // accounts.push({username:'rajat',balance:20000});
 // storage.setItemSync('accounts',accounts);
-console.log(accounts);
+//console.log(accounts);
+
+function createAccount(account) {
+    var accounts=storage.getItemSync('accounts');
+    if(typeof accounts==='undefined'){
+        accounts=[];
+    }
+    accounts.push(account);
+    storage.setItemSync('accounts',accounts);
+    return account;
+}
+
+function getAccount(accountName) {
+    var accounts=storage.getItemSync('accounts');
+    var matchedAccount;
+    accounts.forEach(function(account) {
+        if(account.name === accountName){
+            matchedAccount=account;
+        }
+    });
+
+    return matchedAccount;
+}
+
+//storage.setItemSync('accounts',[{name:"rajat",username:'rjtbansal',password:'rjt123'}]);
+//createAccount({name:'facebook',username:'fb123',password:'password123'});
+//createAccount({name:'rajat',username:'rjtbansal',password:'password123'});
+
+
+var retrieveAccount=getAccount('facebook');
+console.log(retrieveAccount);
+
