898b4a7f19161fa7fe16a1f15d6e56a37ed6fa7a
rjtbansal <rjtbansal@yahoo.com>
2016-08-11 21:08:58 -0700

    add post for todo

--- toDo/server.js
+++ toDo/server.js
@@ -1,22 +1,12 @@
 var express=require('express');
 var app=express();
-var todos=[{
-    id:1,
-    description: 'Do laundry',
-    done:false
-},
-{
-    id:2,
-    description:'Buy grocery',
-    done:false 
-},
-{
-    id:3,
-    description:'Go to spa',
-    done:true 
-}];
+var body_parser=require('body-parser');
+var todos=[];
+var todo_next_id=0;
+
 var PORT=process.env.PORT || 3000; 
 
+app.use(body_parser.json()); //attaching middleware to our app 
 app.get('/',function(req,res){
     res.send('Todo app');
 });
@@ -45,6 +35,15 @@ app.get('/todos/:id',function(req,res){
     
 
     res.send('Todo requested by id: '+req.params.id);
+});
+
+//api: post todo
+app.post('/todos',function(req,res){
+    var body=req.body;
+    body.id=todo_next_id;
+    todo_next_id++;
+    todos.push(body);
+    res.json(body);
 });
 
 app.listen(PORT, function(){
