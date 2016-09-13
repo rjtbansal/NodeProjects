a5d6498c716a11766aa80a8d7bc1b9eb42198c7e
Rajat Bansal <Rajat_Bansal@Trimble.com>
2016-09-13 10:24:10 -0700

    added socket io connection

--- sockets/server.js
+++ sockets/server.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app=express();
 var http = require('http').Server(app); //tells node to start a new server and use this express app as boiler plate..so whatever express app
 //is listening to the server is listening to as well
-
+var io=require('socket.io')(http);
+io.on('connection',function(){
+    console.log("User connected via socket io");
+});
 app.use(express.static(__dirname+'/public'));
 
 http.listen(PORT, function(){
