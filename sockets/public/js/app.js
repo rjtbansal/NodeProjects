bb9a9639e8deba9497878f0d376e3b8dc5613f7c
Rajat Bansal <Rajat_Bansal@Trimble.com>
2016-09-13 15:12:06 -0700

    added form and sending messages back and forth

--- sockets/public/js/app.js
+++ sockets/public/js/app.js
@@ -10,3 +10,12 @@ socket.on('connect', function(){
 socket.on('message', function(message){
     console.log('New message from server --> '+message.text); //text was are key in the message object(check server.js)
 });
+
+//Handle message submit through form
+$('#msg-form').on('submit', function(e){
+    e.preventDefault();
+    socket.emit('message',{
+        text: $('#msg-form').find('input[name=message]').val()
+    });
+    $('#msg-form').find('input[name=message]').val('');
+});
