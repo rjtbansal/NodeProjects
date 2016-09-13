5e69007ff5de22dc1bc2e994da41b4e4d53e8bed
rjtbansal <rjtbansal@yahoo.com>
2016-09-12 20:35:18 -0700

    initial commit

---
+++ sockets/server.js
@@ -0,0 +1,11 @@
+var PORT=process.env.PORT || 3000;
+var express = require('express');
+var app=express();
+var http = require('http').Server(app); //tells node to start a new server and use this express app as boiler plate..so whatever express app
+//is listening to the server is listening to as well
+
+app.use(express.static(__dirname+'/public'));
+
+http.listen(PORT, function(){
+    console.log('Server started at port: '+PORT);
+});
